d3d9adbfab4fef1685ab39e211a17b0cd9eb8253
skittlecamp <81906603+skittlecamp@users.noreply.github.com>
2025-10-14 21:34:57 +0200

    ahoj

--- src/main/java/org/example/pazduolingo/HelloApplication.java
+++ src/main/java/org/example/pazduolingo/HelloApplication.java
@@ -15,6 +15,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        System.out.println("Hello world");
+        System.out.println(true);
 
     }
 
